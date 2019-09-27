952babeff99dfe211780d628740175ba80626858
gaorock530 <gaorock530@hotmail.com>
2019-09-27 21:57:43 +0800

    config

--- router/wxlogin.js
+++ router/wxlogin.js
@@ -2,26 +2,23 @@ const axios = require('axios');
 
 module.exports = (app) => {
 
-  // user Register
-  app.post('/wxlogin/getcode', async (req, res) => {
-    const appid = 'wx09fc8bca51c925c7';
-    const redirect_uri = encodeURIComponent('https://www.yingxitech.com/login');
-    const scope = 'snsapi_userinfo';
-    const state = '123abc';
+  app.get('/wxlogin/MP_verify_FdS96m4Og6Nb5Yrw.txt', (req, res) => {
+    res.send('FdS96m4Og6Nb5Yrw');
+  })
+  // wxLogin Step 1
+  app.get('/wxlogin', async (req, res) => {
+    // const appid = 'wx09fc8bca51c925c7';
+    // const redirect_uri = encodeURIComponent('https://www.yingxitech.com/login');
+    // const scope = 'snsapi_userinfo';
+    // const state = '123abc';
     // const url = `https://open.weixin.qq.com/connect/qrconnect?appid=${appid}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&state=${state}#wechat_redirect`;
-    const url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&state=${state}#wechat_redirect`;
+    // const url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&state=${state}#wechat_redirect`;
 
-    let responseObj = {res: "/wxlogin/getcode", url};
-    try {
-      const response = await axios.get(url);
-      console.log('Success:', response);
-      responseObj.data = response.data;
-    } catch (error) {
-      responseObj.error = error;
-      console.log('Error:', error);
-    }
+    let responseObj = {res: "/wxlogin", domin: 'api.yingxitech.com'};
+    console.log(responseObj);
+    console.log(req.params);
 
-    res.send(JSON.stringify(responseObj));
+    res.redirect('https://yingxitech.com/baoming');
   });
 
   app.get('/login', (req, res) => {
