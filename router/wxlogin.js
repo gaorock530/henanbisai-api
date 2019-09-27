1c01db49255b6969fcf97864f74256dd4fcceaee
gaorock530 <gaorock530@hotmail.com>
2019-09-27 19:01:13 +0800

    config

--- router/wxlogin.js
+++ router/wxlogin.js
@@ -10,13 +10,13 @@ module.exports = (app) => {
     // const url = `https://open.weixin.qq.com/connect/qrconnect?appid=${appid}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&state=${state}#wechat_redirect`;
     const url = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appid}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&state=${state}#wechat_redirect`;
 
-    let responseObj;
+    let responseObj = {res: "/wxlogin/getcode"};
     try {
       const response = await axios.get(url);
       console.log('Success:', response);
-      responseObj = response.data;
+      responseObj.data = response.data;
     } catch (error) {
-      responseObj = error;
+      responseObj.error = error;
       console.error('Error:', error);
     }
 
