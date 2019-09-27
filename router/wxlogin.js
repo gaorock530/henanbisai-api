870a4698c6172b1c58179d3a9dd780e8ebc7d8c9
gaorock530 <gaorock530@hotmail.com>
2019-09-27 22:18:41 +0800

    api

--- router/wxlogin.js
+++ router/wxlogin.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
   // wxLogin Step 1
   app.get('/wxlogin', async (req, res) => {
     // const appid = 'wx09fc8bca51c925c7';
-    // const redirect_uri = encodeURIComponent('https://www.yingxitech.com/login');
+    // const redirect_uri = encodeURIComponent('https://api.yingxitech.com/wxlogin?type=baoming');
     // const scope = 'snsapi_userinfo';
     // const state = '123abc';
     // const url = `https://open.weixin.qq.com/connect/qrconnect?appid=${appid}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&state=${state}#wechat_redirect`;
@@ -17,6 +17,21 @@ module.exports = (app) => {
     let responseObj = {res: "/wxlogin", domin: 'api.yingxitech.com'};
     console.log(responseObj);
     console.log(req.query);
+    if (!req.query.code) {
+      res.send('发生错误，请关闭本页面，重新进入！');
+      return res.end();
+    }
+
+    const appid = 'wx09fc8bca51c925c7';
+    const appsecret = '71372b2b8883842e519485e0da99432d';
+    const code = req.query.code;
+    const token_url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appid}&secret=${appsecret}&code=${code}&grant_type=authorization_code`;
+    try {
+      const access_token_response = await axios.get(token_url); 
+      console.log(access_token_response.data);
+    }catch(e) {
+
+    }
 
     res.redirect('https://yingxitech.com/baoming');
   });
