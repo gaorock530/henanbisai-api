eb0a57b10ba91570e9734afac9f236ec1c76a62d
gaorock530 <gaorock530@hotmail.com>
2019-09-28 21:01:08 +0800

    wxconfig

--- router/wxlogin.js
+++ router/wxlogin.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const USER = require('../models/user');
 const useragent = require('useragent');
+const ConvertUTCTimeToLocalTime = require('../helper/timezone');
 
 module.exports = (app) => {
 
