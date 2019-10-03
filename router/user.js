ff592ded15d32f8ed11e788cf2d612ca5d2d18c3
gaorock530 <gaorock530@hotmail.com>
2019-10-03 15:08:03 +0800

    fix wxlogin callback

--- router/user.js
+++ router/user.js
@@ -10,30 +10,30 @@ module.exports = (app) => {
   });
 
   // user Login
-  app.post('/login', async (req, res) => {
-    console.log('POST: /login')
-    const agent = useragent.parse(req.headers['user-agent']);
-    const client = agent.os.toString() + '&' + agent.device.toString() + '&' + agent.toAgent();
-    const ip = getClientIP(req);
-    let user, token;
-    if (req.body.token) {
-      // token
-      console.log('via token')
-      user = await USER.verifyToken(req.body.token, ip, client);
-    } else if (req.body.openid) {
-      // openid
-      console.log('via openid')
-      user = await USER.findOne({openid: req.body.openid});
-      token = await user.generateAuthToken(ip, client, 60*24*7);
-      user.token = token;
-    } else {
-      console.log('no req.body', req.body)
-    }
-    console.log(user);
-    if (!user) return res.json({err: 'null'});
+  // app.post('/login', async (req, res) => {
+  //   console.log('POST: /login')
+  //   const agent = useragent.parse(req.headers['user-agent']);
+  //   const client = agent.os.toString() + '&' + agent.device.toString() + '&' + agent.toAgent();
+  //   const ip = getClientIP(req);
+  //   let user, token;
+  //   if (req.body.token) {
+  //     // token
+  //     console.log('via token')
+  //     user = await USER.verifyToken(req.body.token, ip, client);
+  //   } else if (req.body.openid) {
+  //     // openid
+  //     console.log('via openid')
+  //     user = await USER.findOne({openid: req.body.openid});
+  //     token = await user.generateAuthToken(ip, client, 60*24*7);
+  //     user.token = token;
+  //   } else {
+  //     console.log('no req.body', req.body)
+  //   }
+  //   console.log(user);
+  //   if (!user) return res.json({err: 'null'});
     
-    res.json(user)
-  })
+  //   res.json(user)
+  // })
 
   // user Logout
   app.post('/logout', (req, res) => {
@@ -48,9 +48,9 @@ module.exports = (app) => {
   // Get User
   app.post('/user', async (req, res) => {
     console.log('POST: /user')
-    // req.body.openid
-    if (!req.body.openid) return res.json({user: null});
-    const user = await USER.findOne({openid: req.body.openid});
+    // req.body.unionid
+    if (!req.body.unionid) return res.json({user: null});
+    const user = await USER.findOne({unionid: req.body.unionid});
     console.log(user)
     if (!user) return res.json({user: null});
     res.json(user)
