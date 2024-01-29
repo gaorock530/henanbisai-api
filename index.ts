f227211e2c13feadc2a0e416a223159a68e2ba8a
gaorock530 <gaorock530@hotmail.com>
2024-01-29 21:50:17 +0800

    parse

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'
 import fs from 'fs'
-import {
-  readFile,
-  writeFile
-} from 'node:fs/promises'
+// import {
+//   readFile,
+//   writeFile
+// } from 'node:fs/promises'
 import cookieParser from 'cookie-parser'
 import parse from 'node-html-parser'
 import puppeteer from 'puppeteer-core'
@@ -78,202 +78,16 @@ app.get('/parse', async (req, res) => {
   try {
     const q = req.query.q
 
-    if (q) {
-      // get html page content
-      const htmlRes = await axios.get(q.toString(), {
-        headers: {
-          Cookie: lan_cookie,
-          'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-        },
-      })
-      if (htmlRes.status !== 200) throw Error(htmlRes.statusText)
-      temphtml.string = htmlRes.data
-    } else {
-      const pagehtml = await readFile('page.txt')
-      temphtml.string = pagehtml.toString()
-    }
+    if (!q) return res.json({ error: 'no q' })
+    const parsed = await parseDetailPage(q as string, false)
 
-
-    // parse response html string into dom
-    const dom = parse(temphtml.string)
-
-    // get title & tags
-    let title = dom.querySelector('.article-title')?.innerText
-
-    let year = null
-
-    const sYear = title?.match(/\[(\d{4})\]/)
-    if (sYear && sYear[0]) {
-      console.log({ sYear })
-      // if (sYear[0].length === 6) year = sYear[0].slice(1, -1)
-      year = sYear[1].trim()
-      title = title?.replace(/[\[?](\d{4})[\]?]/, '')
-    }
-
-    const tag = title?.match(/[\[\《].+[\]\》]/ig)
-    let size = null, tags
-    if (tag) {
-      title = title?.replace(tag[0], '')
-      console.log(title)
-      const lis = tag[0].slice(1, -1).split(' ')
-      const item = lis.filter(t => t.trim().match(/\d+\.?\d+(M|MB|G|GB){1}/ig))
-      console.log({ item })
-      if (item) size = item[0].replace(/([M|G]$)/, "$1B")
-      tags = lis.filter(t => !t.match(/\d+\.?\d+(M|MB|G|GB){1}/ig))
-    }
-
-
-    if (!year) {
-      const findYear = title?.match(/\s\d{4}\s?|\]?$/)
-      console.log({ findYear })
-      if (findYear && findYear[0]) {
-        year = findYear[0].trim()
-        // if (title && (Number(findYear[1]) + findYear[0].length) > title.length) title = title?.replace(/\s\[?\d{4}\s|\]?$/, '')
-      }
-    }
-
-
-    // get content
-    let introductionNodes = dom.querySelectorAll(
-      'div.article-content>p'
-    )
-
-    if (introductionNodes.length === 0) introductionNodes = dom.querySelectorAll(
-      'div.article-content>div'
-    )
-
-
-    let htmlContent = ''
-    for (const p of introductionNodes) {
-      let need = true
-      for (const node of p.childNodes) {
-        // 1 - ELEMENT_NODE, 3 - TEXT_NODE
-        if (node.toString().match(/\<img/ig)) {
-          need = false
-          break
-        }
-      }
-      if (need) htmlContent = htmlContent.concat(`<p>${p.innerHTML}</p>`)
-    }
-
-
-    // get all pictures 
-    let photos = dom.querySelectorAll('.article-content img')
-    const urls = []
-    for (const img of photos) {
-      if (img.getAttribute('src')) urls.push(img.getAttribute('src'))
-    }
-
-    // get download link
-    const downlaod_link_inital = dom.querySelector('.erphpdown-box .erphpdown-cart a.down.bought')
-
-
-    res.json({
-      title: title?.trim(),
-      year,
-      tags,
-      size,
-      content: htmlContent,
-      images: urls,
-      link: downlaod_link_inital ? downlaod_link_inital.getAttribute('href') : null
-    })
+    res.json(parsed)
   } catch (e: any) {
     res.json({ error: e.toString() })
+    return e
   }
+
 })
-
-
-app.get('/read', async (req, res) => {
-  try {
-    // get html page content
-    const htmlRes = await axios.get('https://www.lgych.com/70527.html', {
-      headers: {
-        Cookie: lan_cookie,
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-      },
-    })
-
-    if (htmlRes.status !== 200) throw Error(htmlRes.statusText)
-    await writeFile('page.txt', htmlRes.data)
-    temphtml.string = htmlRes.data
-    res.send(htmlRes.data)
-  } catch (e: any) {
-    res.json({ error: e.toString() })
-  }
-})
-
-app.get('/get', async (req, res) => {
-  const q = req.query.q
-  if (!q) {
-    res.send('no q')
-    return
-  }
-  try {
-    // get html page content
-    const htmlRes = await axios.get(q.toString(), {
-      headers: {
-        Cookie: lan_cookie,
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-      },
-    })
-
-    console.log(htmlRes)
-
-    if (htmlRes.status !== 200) throw Error(htmlRes.statusText)
-    await writeFile('page.txt', htmlRes.data)
-    temphtml.string = htmlRes.data
-    res.send(htmlRes.data)
-  } catch (e: any) {
-    res.json({ error: e.toString() })
-  }
-})
-
-app.get('/redirect', async (req, res) => {
-  const q = req.query.q
-  if (!q) {
-    res.send('no q')
-    return
-  }
-  try {
-    const axiosInstance = axios.create();
-    axiosInstance.defaults.maxRedirects = 1; // Set to 0 to prevent automatic redirects
-    const htmlRes = await axiosInstance.get(q.toString(), {
-      headers: {
-        // ':authority': 'www.lgych.com',
-        // ':method': 'GET',
-        // ':path': '/wp-content/plugins/erphpdown/download.php?postid=38608&key=1&index=',
-        // ':scheme': 'https',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6',
-        'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-        'Sec-Ch-Ua-Mobile': '?0',
-        'Sec-Ch-Ua-Platform': '"Windows"',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'none',
-        'Sec-Fetch-User': '?1',
-        'Upgrade-Insecure-Requests': '1',
-        Cookie:
-          'zh_choose=s; wordpress_sec_c31b215c0db6cdb6478d719de4022ec2=punkhead%7C1703775643%7CvbxtrsdrRdTOAM10NFH7W7Mxw8lMejv8wAq6OkFRC14%7C2caed6a352b75c92aefe5f0c48fb80734a4a66ff50895c47217d6ee6d6cc4134; PHPSESSID=5dsbimkv6pu4rgnga42rthbsco; __51vcke__K4Eg8SGElFhlWvHA=c02e95a2-c4ca-5172-811e-0ea49a7d1a97; __51vuft__K4Eg8SGElFhlWvHA=1685164511733; _ga_JPNTDP3XJE=GS1.1.1685766273.15.0.1685766273.0.0.0; Hm_lvt_20dbded09f6ac464b84faec7ab3a278b=1691308021; Hm_lpvt_20dbded09f6ac464b84faec7ab3a278b=1693593774; zh_choose=s; mbt_theme_night=1; _ga=GA1.1.519116480.1643790333; zanIds=180; wp-settings-time-26307=1702105524; wp-settings-26307=editor%3Dtinymce; Hm_lvt_7233eaff4ea4aa81ba9933f3a0e42474=1702304299; wordpress_logged_in_c31b215c0db6cdb6478d719de4022ec2=punkhead%7C1703775643%7CvbxtrsdrRdTOAM10NFH7W7Mxw8lMejv8wAq6OkFRC14%7C6d45414ceba297077303c2521dec11582aa102b15e3b1efded1c95dd611b9c41; erphp_login_tips=0; mycred_site_visit=1; __51uvsct__K4Eg8SGElFhlWvHA=80; Hm_lpvt_7233eaff4ea4aa81ba9933f3a0e42474=1702729886; __vtins__K4Eg8SGElFhlWvHA=%7B%22sid%22%3A%20%229460ee28-0341-5727-b407-a403883a1339%22%2C%20%22vd%22%3A%208%2C%20%22stt%22%3A%202056956%2C%20%22dr%22%3A%20195620%2C%20%22expires%22%3A%201702731685719%2C%20%22ct%22%3A%201702729885719%7D; _ga_EYK4RPLNVD=GS1.1.1702727828.13.1.1702729885.0.0.0',
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-      },
-    })
-    console.log(htmlRes.headers)
-    if (htmlRes.status !== 200) throw Error(htmlRes.statusText)
-    await writeFile('page.txt', htmlRes.data)
-    temphtml.string = htmlRes.data
-    res.send(htmlRes.data)
-  } catch (e: any) {
-    res.json({ error: e.toString() })
-  }
-})
-
-
 
 app.get('/share', async (req, res) => {
   let page
@@ -298,11 +112,6 @@ app.get('/share', async (req, res) => {
     const link = await copySelector?.evaluate(el => el.value);
     const codeSelector = await page.waitForSelector('.wp-s-share-to-link__link-pwd input')
     const pwd = await codeSelector?.evaluate(el => el.value);
-
-
-
-
-
 
 
     const pic = await page.screenshot({ encoding: 'base64' })
@@ -476,7 +285,7 @@ async function parseDetailPage(url: string, free: boolean) {
     // get title & tags
     let title = dom.querySelector('.article-title')?.innerText
 
-    let year = null
+    let year = null, bad = null
 
     const sYear = title?.match(/[\[?](\d{4})[\]?]/)
     if (sYear && sYear[0]) {
@@ -491,7 +300,8 @@ async function parseDetailPage(url: string, free: boolean) {
       title = title?.replace(tag[0], '')
       const lis = tag[0].slice(1, -1).split(' ')
       const item = lis.filter(t => t.trim().match(/\d+\.?\d+(M|MB|G|GB){1}/ig))
-      if (item) size = item[0].replace(/([M|G]$)/, "$1B")
+      if (item && item[0]) size = item[0].replace(/([M|G]$)/, "$1B")
+      else bad = true
       tags = lis.filter(t => !t.match(/\d+\.?\d+(M|MB|G|GB){1}/ig))
     }
 
@@ -548,7 +358,8 @@ async function parseDetailPage(url: string, free: boolean) {
       size,
       content: htmlContent,
       images: urls,
-      link: downlaod_link_inital ? downlaod_link_inital.getAttribute('href') : null
+      link: downlaod_link_inital ? downlaod_link_inital.getAttribute('href') : null,
+      bad
     }
   } catch (e: any) {
     console.log(e.toString())
